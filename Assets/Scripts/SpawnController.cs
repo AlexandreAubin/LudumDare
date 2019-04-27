e9faf7b61df66e572b8b9aca2ebc022b80d56ee9
Mindalajinka <ld.jonathan.mottin@gmail.com>
2019-04-27 18:07:33 -0400

    Add ennemy wave

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -5,21 +5,73 @@ using UnityEngine;
 public class SpawnController : MonoBehaviour
 {
     public Transform spawnPoints;
-    public GameObject enemyPrefab;
+    public enemyController enemyPrefab1;
+    public enemyController enemyPrefab2;
+    public int HowmanyNME;
+
+    public int nbrNME;
 
     // Start is called before the first frame update
     void Start()
     {
-        int random = Random.Range(0, spawnPoints.childCount);
+        SpawnNMEBuff();
+        /*int random = Random.Range(0, spawnPoints.childCount);
         Instantiate(enemyPrefab, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
 
         random = Random.Range(0, spawnPoints.childCount);
-        Instantiate(enemyPrefab, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
+        Instantiate(enemyPrefab, spawnPoints.GetChild(random).transform.position, Quaternion.identity);*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.P))
+        {
+            nbrNME--;
+        }
+        if (nbrNME <= 0)
+        {
+            SpawnNMESaiyen();
+        }
+    }
+
+    //Fonctions
+    public void SpawnNMEBuff()
+    {
+        for (int i = 0; i < Random.Range(1, HowmanyNME); i++)
+        {
+            int random = Random.Range(0, spawnPoints.childCount);
+            enemyController temp = Instantiate(enemyPrefab1, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
+            nbrNME++;
+            //temp.SetSpawnController(this);
+        }
+    }
+
+    public void SpawnNMESaiyen()
+    {
+        for (int i = 0; i < Random.Range(1, HowmanyNME); i++)
+        {
+            int random = Random.Range(0, spawnPoints.childCount);
+            Instantiate(enemyPrefab2, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
+            nbrNME++;
+        }
+    }
+
+    public void SpawnNMEMIX()
+    {
+        for (int i = 0; i < Random.Range(1, HowmanyNME); i++)
+        {
+            if (Random.Range(0,1)==0)
+            {
+                int random = Random.Range(0, spawnPoints.childCount);
+                Instantiate(enemyPrefab1, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
+            }
+            else
+            {
+                int random = Random.Range(0, spawnPoints.childCount);
+                Instantiate(enemyPrefab2, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
+            }
+            nbrNME++;
+        }
     }
 }
