af3ffa4ee73f3a126e6b40547b37de4c6872f070
Alexandre <alexandre.aubin@hotmail.com>
2019-04-27 02:44:36 -0400

    rebase master

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,24 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
+    public const string PLAYER_CLASS = "Player";
+
     public GameObject[] obstacles;
 
-    public GameObject player;
+    public Player player;
 
     // Start is called before the first frame update
     void Start()
     {
         obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
-        player = GameObject.FindGameObjectWithTag("Player");
-
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent(PLAYER_CLASS) as Player;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public bool isThereCollision(Vector2Int position)
+    {
+        foreach(GameObject obstacle in obstacles)
+        {
+            Vector2Int obstaclePoint = new Vector2Int((int)Math.Round(obstacle.transform.position.x), (int)Math.Round(obstacle.transform.position.y));
+            Vector2Int obstacleRect = new Vector2Int((int)Math.Round(obstacle.transform.localScale.x), (int)Math.Round(obstacle.transform.localScale.y));
+            RectInt rect = new RectInt(obstaclePoint, obstacleRect);
+            if(rect.Contains(position))
+            {
+                return true;
+            }
+            /*if((obstaclePoint.x == position.x || (obstaclePoint.x + obstacleRect.x) == position.x) && ( obstaclePoint.y == position.y || (obstaclePoint.y + obstacleRect.y) == position.y))
+            {
+                return true;
+            }*/
+        }
+
+        return false;
     }
 }
