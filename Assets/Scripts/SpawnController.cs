518f59e40ef14b657468b8d9c16c4a68197f45cb
Mindalajinka <ld.jonathan.mottin@gmail.com>
2019-04-27 19:49:51 -0400

    continue wave of ennemies

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class SpawnController : MonoBehaviour
 {
     public Transform spawnPoints;
+    public Transform wayPoints;
     public enemyController enemyPrefab1;
     public enemyController enemyPrefab2;
     public int HowmanyNME;
-
     public int nbrNME;
+
+    private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         SpawnNMEBuff();
         /*int random = Random.Range(0, spawnPoints.childCount);
         Instantiate(enemyPrefab, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
@@ -43,7 +46,8 @@ public class SpawnController : MonoBehaviour
             int random = Random.Range(0, spawnPoints.childCount);
             enemyController temp = Instantiate(enemyPrefab1, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
             nbrNME++;
-            //temp.SetSpawnController(this);
+            temp.player = player;
+            temp.destinations = wayPoints;
         }
     }
 
@@ -52,8 +56,10 @@ public class SpawnController : MonoBehaviour
         for (int i = 0; i < Random.Range(1, HowmanyNME); i++)
         {
             int random = Random.Range(0, spawnPoints.childCount);
-            Instantiate(enemyPrefab2, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
+            enemyController temp = Instantiate(enemyPrefab2, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
             nbrNME++;
+            temp.player = player;
+            temp.destinations = wayPoints;
         }
     }
 
@@ -64,12 +70,16 @@ public class SpawnController : MonoBehaviour
             if (Random.Range(0,1)==0)
             {
                 int random = Random.Range(0, spawnPoints.childCount);
-                Instantiate(enemyPrefab1, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
+                enemyController temp = Instantiate(enemyPrefab1, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
+                temp.player = player;
+                temp.destinations = wayPoints;
             }
             else
             {
                 int random = Random.Range(0, spawnPoints.childCount);
-                Instantiate(enemyPrefab2, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
+                enemyController temp = Instantiate(enemyPrefab2, spawnPoints.GetChild(random).transform.position, Quaternion.identity);
+                temp.player = player;
+                temp.destinations = wayPoints;
             }
             nbrNME++;
         }
