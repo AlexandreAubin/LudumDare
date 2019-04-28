c133e08ea38bb5e46736185bb1f4b8e4e2c41d2c
Derek Capodagli <derek.capodagli.1@ens.etsmtl.ca>
2019-04-28 10:16:32 -0400

    Commit to accept pull

---
+++ Assets/Scripts/LavaTileController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaTileController : MonoBehaviour
+{
+    public GameObject playerCharacter;
+
+    private GameObject[] lava;
+    private Player player;
+    private Vector2Int position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = player = GameObject.FindGameObjectWithTag("Player").GetComponent("Player") as Player;
+        lava = GameObject.FindGameObjectsWithTag("lava");
+
+        position = new Vector2Int((int)playerCharacter.transform.position.x, (int)playerCharacter.transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*foreach (GameObject tile in lava)
+        {
+            //The size of the current object on the UI
+            var UISize = gameObject.GetComponent<SpriteRenderer>().bounds.size;
+            Rect playerRect = new Rect((int)Math.Round(position.x - (UISize.x / 2)), (int)Math.Round(position.y - (UISize.y / 2)), (int)Math.Round(UISize.x), (int)Math.Round(UISize.y));
+            Vector2Int lavaTilePoint = new Vector2Int((int)Math.Round(tile.transform.position.x - (tile.transform.localScale.x / 2)), (int)Math.Round(tile.transform.position.y - (tile.transform.localScale.y / 2)));
+            Vector2Int lavaTileRect = new Vector2Int((int)Math.Round(tile.transform.localScale.x), (int)Math.Round(tile.transform.localScale.y));
+            Rect lavaRect = new Rect(lavaTilePoint, lavaTileRect);
+
+            if (playerRect.Overlaps(lavaRect))
+            {
+                player.CurrentHealth -= 3;
+                StartCoroutine(DamageOverTime(3));
+                print("hello");
+            }
+        }*/
+    }
+
+    private IEnumerator DamageOverTime(int seconds)
+    {
+        float pauseEndTime = Time.realtimeSinceStartup + seconds;
+
+        while (Time.realtimeSinceStartup < pauseEndTime)
+        {
+            yield return null; // Attend un frame
+        }
+    }
+
+}
